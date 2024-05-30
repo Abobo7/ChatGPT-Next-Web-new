f26857214d71f22c6ccddcd080e25e1de61760f3
Abobo7 <2557826346@qq.com>
2024-05-30 11:11:02 +0800

    个性化内容

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          牧之的AI小站
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          长亭外，古道边，芳草碧连天
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
